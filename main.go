94223aae0d94c6380adc878768566422d63d0759
Lanre <lanre@Haliax0.local>
2025-05-11 15:41:55 -0500

    little funcy func

--- main.go
+++ main.go
@@ -5,7 +5,48 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	reader := io.Reader(f)
+	buf := make([]byte, 8)
+	currentLine := ""
+	c := make(chan string)
+
+	go func() {
+		defer close(c)
+		defer f.Close()
+
+		for {
+			n, err := reader.Read(buf)
+			if n > 0 {
+				chunk := string(buf[:n])
+				lines := strings.Split(chunk, "\n")
+				for i, line := range lines {
+					if i == 0 {
+						currentLine += line
+					} else {
+						c <- currentLine
+						currentLine = line
+					}
+				}
+			}
+			if err == io.EOF {
+				if currentLine != "" {
+					c <- currentLine
+				}
+				break
+			}
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+	}()
+	return c
+}
 
 func main() {
 	msg, err := os.Open("messages.txt")
@@ -13,21 +54,9 @@ func main() {
 		fmt.Printf("%v", err)
 	}
 
-	reader := io.Reader(msg)
-
-	buf := make([]byte, 8)
-	for {
-		n, err := reader.Read(buf)
-		if n > 0 {
-			fmt.Printf("read: %s\n", buf[:n])
-		}
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
+	msgChan := getLinesChannel(msg)
+	for msg := range msgChan {
+		fmt.Printf("read: %s\n", msg)
 	}
 
 }
